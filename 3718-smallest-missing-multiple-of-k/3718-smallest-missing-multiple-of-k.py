c086075bba7ab9f47d21fe0384e6f4ac91ecce86
Akshaykumar Santi <santiakshay248@gmail.com>
2026-08-08 10:43:43 +0530

    Time: 0 ms (100%), Space: 19.3 MB (48.84%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -1,16 +1,8 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
-        ans = True
         i = 1
-        while ans:
+        while True:
             if i * k not in nums:
-                ans = False
                 return i * k
             else:
                 i = i + 1
-
-            
-
-                
-
-        
