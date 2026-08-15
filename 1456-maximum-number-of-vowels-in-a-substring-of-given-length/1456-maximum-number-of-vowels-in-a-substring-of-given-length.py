b8d3f2cb00d2228229404ab5edca1e561e082ee9
Akshaykumar Santi <santiakshay248@gmail.com>
2026-08-15 09:46:49 +0530

    Time: 83 ms (16.33%), Space: 19.9 MB (29.3%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -3,18 +3,19 @@ class Solution:
         v = ['a','e','i','o','u']
         ans = 0
         count = 0
+        i = 0
 
-        for i in range(k):
-            if s[i] in v:
+        for j in range(len(s)):
+            if s[j] in v:
                 count += 1
-        ans = count
 
-        for i in range(k,len(s)):
-            if s[i] in v:
-                count += 1
-            if s[i-k] in v:
-                count -= 1
-            if count > ans:
-                ans = count
+            if j-i+1 == k:
+                if count > ans:
+                    ans = count
 
-        return ans   
+                if s[i] in v:
+                    count -= 1
+
+                i = i + 1
+        
+        return ans
